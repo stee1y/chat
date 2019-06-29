d07ed3ea22f17b1fa7e6a8ade6d2d53ed16098db
Ivan Eremeev <stee15454@yandex.ru>
2019-06-29 15:02:35 +0500

    test3 chat

--- plugins/socket.js
+++ plugins/socket.js
@@ -4,7 +4,7 @@ import VueSocketIO from 'vue-socket.io'
 export default function ({store}) {
     Vue.use(new VueSocketIO({
         debug: false,
-        connection: 'http://localhost:3000',
+        connection: 'https://x-chats.herokuapp.com/',
         vuex: {
             store,
             actionPrefix: 'SOCKET_',
